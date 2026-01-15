36ea0d13edd107552b24e85714258af18f1f815e
JaJo EkiZ <jajoultrapunk@gmail.com>
2026-01-15 01:21:12 -0300

    feat:crea el archivo startup.sh

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. Copiar configuración de Nginx (Desde tu carpeta persistente al sistema)
+cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-available/default
+service nginx reload
+
+# 2. Caché de Laravel
+php /home/site/wwwroot/artisan config:clear
+php /home/site/wwwroot/artisan route:clear
+php /home/site/wwwroot/artisan view:clear
+
+# 3. Enlace simbólico para imágenes
+php /home/site/wwwroot/artisan storage:link
