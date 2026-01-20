e404c8794510351b290a8a253b58b38567391fbb
JaJo EkiZ <jajoultrapunk@gmail.com>
2026-01-20 00:50:25 -0300

    feat: cambia el favicon.ico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cronograma Flexible</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/flexiweek-Iso.png') }}">
     
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
